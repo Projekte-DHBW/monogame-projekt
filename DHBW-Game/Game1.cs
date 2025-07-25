45571a1a8f714ceb13af053e8c1138539ac2d494
nile <nico.l.05.04.03@gmail.com>
2025-07-25 19:02:13 +0200

    temp

--- DHBW-Game/Game1.cs
+++ DHBW-Game/Game1.cs
@@ -8,6 +8,9 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    // The MonoGame logo texture
+    private Texture2D _logo;
+
 
     public Game1()
     {
@@ -29,6 +32,8 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/logo");
+
     }
 
     protected override void Update(GameTime gameTime)
@@ -47,6 +52,15 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        // Begin the sprite batch to prepare for rendering.
+        _spriteBatch.Begin();
+
+        // Draw the logo texture
+        _spriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+
+        // Always end the sprite batch when finished.
+        _spriteBatch.End();
+
 
         base.Draw(gameTime);
     }
