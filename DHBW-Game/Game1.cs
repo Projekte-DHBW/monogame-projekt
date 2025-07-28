c4ad5bbbf239725bdde05f9311718e0280b06786
Maria Lazaridou <lazaridoum.tin24@student.dhbw-heidenheim.de>
2025-07-28 15:25:49 +0200

    dhbw logo und weißer hintergrund

--- DHBW-Game/Game1.cs
+++ DHBW-Game/Game1.cs
@@ -1,53 +1,73 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using MonoGameLibrary;
+ 
 namespace DHBW_Game;
-
-public class Game1 : Game
+ 
+public class Game1 : Core
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
-    public Game1()
+    // The MonoGame logo texture
+    private Texture2D _logo;
+ 
+    public Game1() : base("DHBW", 1280, 720, false)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Window.Title = "DHBW Game";
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+ 
     }
-
+ 
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-
+ 
         base.Initialize();
     }
-
+ 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
         // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/logo");
+       
     }
-
+ 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-            Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
+ 
         // TODO: Add your update logic here
-
+ 
         base.Update(gameTime);
     }
-
+ 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
-
+        GraphicsDevice.Clear(Color.White);
+ 
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin();
+ 
+         // Set position to top-left corner with a small margin
+        Vector2 position = new Vector2(10, 10);
+ 
+        // Scale the logo (z. B. auf 25 % der Originalgröße)
+        float scale = 0.25f;
+ 
+        // Draw the scaled logo in the top-left corner
+        SpriteBatch.Draw(
+            _logo,
+            position,
+            null,           // sourceRectangle
+            Color.White,
+            0f,             // rotation
+            Vector2.Zero,   // origin
+            scale,          // scale
+            SpriteEffects.None,
+            0f              // layerDepth
+        );
+ 
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
+ 
         base.Draw(gameTime);
     }
 }
