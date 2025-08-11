978e56e88012205a7dff6cbaad0ed7b05e460cf2
maren feldmeier <maren.feldmeier@zeiss.com>
2025-08-11 16:24:58 +0200

    completed monogame tutorial

--- Game1.cs
+++ Game1.cs
@@ -1,20 +1,22 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+﻿using monogame_projekt.Scenes;
+using Microsoft.Xna.Framework.Media;
+using MonoGameLibrary;
+using MonoGameGum;
+using MonoGameGum.Forms.Controls;
+
 
 namespace monogame_projekt;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
 
-    public Game1()
+    // The background theme song
+    private Song _themeSong;
+
+
+    public Game1() : base("DHBW Game", 1280, 720, false)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Window.Title = "DHBW Game";
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+
     }
 
     protected override void Initialize()
@@ -22,32 +24,57 @@ public class Game1 : Game
         // TODO: Add your initialization logic here
 
         base.Initialize();
+
+        // Start playing the background music.
+        Audio.PlaySong(_themeSong);
+
+        // Initialize the Gum UI service
+        InitializeGum();
+
+        // Start the game with the title scene.
+        ChangeScene(new TitleScene());
+
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        // Load the background theme music.
+        _themeSong = Content.Load<Song>("audio/theme");
+
     }
 
-    protected override void Update(GameTime gameTime)
+    private void InitializeGum()
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-            Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        // Initialize the Gum service
+        GumService.Default.Initialize(this);
 
-        // TODO: Add your update logic here
+        // Tell the Gum service which content manager to use.  We will tell it to
+        // use the global content manager from our Core.
+        GumService.Default.ContentLoader.XnaContentManager = Core.Content;
 
-        base.Update(gameTime);
+        // Register keyboard input for UI control.
+        FrameworkElement.KeyboardsForUiControl.Add(GumService.Default.Keyboard);
+
+        // Register gamepad input for Ui control.
+        FrameworkElement.GamePadsForUiControl.AddRange(GumService.Default.Gamepads);
+
+        // Customize the tab reverse UI navigation to also trigger when the keyboard
+        // Up arrow key is pushed.
+        FrameworkElement.TabReverseKeyCombos.Add(
+           new KeyCombo() { PushedKey = Microsoft.Xna.Framework.Input.Keys.Up });
+
+        // Customize the tab UI navigation to also trigger when the keyboard
+        // Down arrow key is pushed.
+        FrameworkElement.TabKeyCombos.Add(
+           new KeyCombo() { PushedKey = Microsoft.Xna.Framework.Input.Keys.Down });
+
+        // The assets created for the UI were done so at 1/4th the size to keep the size of the
+        // texture atlas small.  So we will set the default canvas size to be 1/4th the size of
+        // the game's resolution then tell gum to zoom in by a factor of 4.
+        GumService.Default.CanvasWidth = GraphicsDevice.PresentationParameters.BackBufferWidth / 4.0f;
+        GumService.Default.CanvasHeight = GraphicsDevice.PresentationParameters.BackBufferHeight / 4.0f;
+        GumService.Default.Renderer.Camera.Zoom = 4.0f;
     }
 
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
-
-        base.Draw(gameTime);
-    }
 }
