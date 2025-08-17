3ca0eabe335b5b13ccb11291bf368474081f8d43
Leon <baischl.tin24@student.dhbw-heidenheim.de>
2025-08-17 17:41:27 +0200

    - Refactor UI System to separate panels from scenes, similar to the existing separation of the OptionSlider. - Expand Service Locator to allow named instances to be able to have multiple instances of the same type. This is used to load SpriteFonts globally and register them for usage in the game.

--- GameLibrary/ServiceLocator.cs
+++ GameLibrary/ServiceLocator.cs
@@ -6,46 +6,48 @@ namespace GameLibrary;
 /// <summary>
 /// Provides a simple service locator pattern implementation for registering and retrieving services by type.
 /// This static class acts as a central registry for services, allowing dependency injection without direct references.
-/// It uses a dictionary to map service types to their instances.
+/// It uses a dictionary to map service types and optional names to their instances, supporting multiple instances per type.
 /// </summary>
 public static class ServiceLocator
 {
     /// <summary>
-    /// The internal dictionary that stores registered services, keyed by their type.
+    /// The internal dictionary that stores registered services, keyed by a tuple of their type and name.
     /// It is readonly to prevent accidental reassignment, but its contents can be modified.
     /// </summary>
-    private static readonly Dictionary<Type, object> _services = new Dictionary<Type, object>();
+    private static readonly Dictionary<(Type Type, string Name), object> _services = new Dictionary<(Type, string), object>();
 
     /// <summary>
-    /// Registers a service instance of type T in the locator.
-    /// If a service of the same type is already registered, it will be overwritten.
+    /// Registers a service instance of type T in the locator with an optional name.
+    /// If a service of the same type and name is already registered, it will be overwritten.
     /// </summary>
     /// <typeparam name="T">The type of the service to register.</typeparam>
     /// <param name="service">The instance of the service to register.</param>
-    public static void Register<T>(T service)
+    /// <param name="name">Optional name to distinguish multiple instances of the same type. Defaults to empty string.</param>
+    public static void Register<T>(T service, string name = "")
     {
-        // Store the service instance under its type key
-        _services[typeof(T)] = service;
+        // Store the service instance under its type and name key
+        _services[(typeof(T), name)] = service;
     }
 
     /// <summary>
-    /// Retrieves a registered service instance of type T.
+    /// Retrieves a registered service instance of type T by optional name.
     /// Throws an exception if the service is not registered.
     /// </summary>
     /// <typeparam name="T">The type of the service to retrieve.</typeparam>
+    /// <param name="name">Optional name to distinguish multiple instances of the same type. Defaults to empty string.</param>
     /// <returns>The registered service instance.</returns>
-    /// <exception cref="InvalidOperationException">Thrown if no service of type T is registered.</exception>
-    public static T Get<T>()
+    /// <exception cref="InvalidOperationException">Thrown if no service of type T (and name) is registered.</exception>
+    public static T Get<T>(string name = "")
     {
         // Attempt to retrieve the service from the dictionary
-        if (_services.TryGetValue(typeof(T), out var service))
+        if (_services.TryGetValue((typeof(T), name), out var service))
         {
             // Cast and return the service if found
             return (T)service;
         }
 
         // Throw an exception if the service is not found
-        throw new InvalidOperationException($"Service {typeof(T).Name} not registered.");
+        throw new InvalidOperationException($"Service {typeof(T).Name} (name: '{name}') not registered.");
     }
 
     /// <summary>
