c78dac6c45b17af01be65bf0c8ede4794855e1d8
maren feldmeier <maren.feldmeier@zeiss.com>
2025-09-03 17:22:14 +0200

    feat: Add GameObject file for the Door_Closed Sprite
    - Create a Door_Closed.cs file that inherits from GameObject
    - Create in this file a Sprite from the region defined in Door_Closed-definition.xml

---
+++ DHBW-Game/GameObjects/Static_Sprites/Door_Closed.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using GameLibrary;
+using GameLibrary.Entities;
+using GameLibrary.Graphics;
+using GameLibrary.Physics;
+using GameLibrary.Physics.Colliders;
+using GameLibrary.Rendering;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameTutorial;
+
+namespace GameObjects.Static_Sprites.Door_Closed;
+
+public class Door_Closed : GameObject
+{
+
+    public Door_Closed()
+    {
+
+    }
+
+
+    /// <summary>
+    /// Initializes the test character at the given starting position in the world.
+    /// </summary>
+    /// <param name="startingPosition">The position at which the test character should spawn.</param>
+    public override void Initialize(Vector2 position)
+    {
+        base.Initialize(position);
+    }
+
+    public override void LoadContent()
+    {
+        TextureAtlas closedDoorAtlas = TextureAtlas.FromFile(Core.Content, "Static_Sprites/Door/Door_Closed-definition.xml");
+
+        // Create the closed door sprite
+        var closedDoorRegion = closedDoorAtlas.GetRegion("closedDoor");
+        Sprite = new Sprite(closedDoorRegion);
+        Sprite.Scale = new Vector2(4.0f, 4.0f);
+
+        base.LoadContent();
+    }
+
+
+    /// <summary>
+    /// Updates the sprite which is set and also handles the input.
+    /// </summary>
+    /// <param name="gameTime">A snapshot of the timing values for the current update cycle.</param>
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+
+    }
+
+    /// <summary>
+    /// Draws the sprite which is set.
+    /// </summary>
+    public override void Draw()
+    {
+        base.Draw();
+    }
+}
