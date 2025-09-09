90724990a1fd14ba200b764d652578fa55b9529e
maren feldmeier <maren.feldmeier@zeiss.com>
2025-09-09 13:58:32 +0200

    feat: Create GameObject File for the Whiteboard
    - Create a Whiteboard.cs file that inherits from GameObject
    - Create in this file a Sprite from the regions defined in Whiteboard-definition.xml

---
+++ DHBW-Game/GameObjects/Static_Sprites/Whiteboard.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using GameLibrary;
+using GameLibrary.Entities;
+using GameLibrary.Graphics;
+using GameLibrary.Physics;
+using GameLibrary.Physics.Colliders;
+using GameLibrary.Rendering;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameTutorial;
+
+namespace GameObjects.Static_Sprites.Whiteboard;
+
+public class Whiteboard : GameObject
+{
+    private string _spriteRegionName;
+
+    public Whiteboard() : this("whiteboard")
+    {
+
+    }
+
+    public Whiteboard(string spriteRegionName)
+    {
+        _spriteRegionName = spriteRegionName;
+    }
+
+    /// <summary>
+    /// Initializes the test character at the given starting position in the world.
+    /// </summary>
+    /// <param name="startingPosition">The position at which the test character should spawn.</param>
+    public override void Initialize(Vector2 position)
+    {
+        base.Initialize(position);
+    }
+
+    public override void LoadContent()
+    {
+        TextureAtlas whiteboardAtlas = TextureAtlas.FromFile(Core.Content, "Static_Sprites/Whiteboard-definition.xml");
+
+        // Create the closed door sprite
+        var whiteboardRegion = whiteboardAtlas.GetRegion(_spriteRegionName);
+        Sprite = new Sprite(whiteboardRegion);
+        Sprite.Scale = new Vector2(8.0f, 8.0f);
+
+        base.LoadContent();
+    }
+
+
+    /// <summary>
+    /// Updates the sprite which is set and also handles the input.
+    /// </summary>
+    /// <param name="gameTime">A snapshot of the timing values for the current update cycle.</param>
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+
+    }
+
+    /// <summary>
+    /// Draws the sprite which is set.
+    /// </summary>
+    public override void Draw()
+    {
+        base.Draw();
+    }
+}
