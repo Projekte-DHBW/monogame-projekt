489ef55cf37dc024584739d8fd0527c94af4a245
maren feldmeier <maren.feldmeier@zeiss.com>
2025-09-15 18:27:05 +0200

    feat: Change final professor to Goal

--- DHBW-Game/Levels/Level.cs
+++ DHBW-Game/Levels/Level.cs
@@ -157,8 +157,8 @@ namespace DHBW_Game.Levels
                             fireExtinguisher.Initialize(new Vector2(x * Tiles.TILE_SIZE, y * Tiles.TILE_SIZE - 15));
                             BackgroundSprites.Add(fireExtinguisher);
                             break;
-                        case 'E': // End, Final Professor
-                            enemy = new FinalProfessor(mass: 1.5f, isElastic: false);
+                        case 'G': // End, Goal
+                            enemy = new Goal(mass: 1.5f, isElastic: false);
                             enemy.Initialize(new Vector2(x * Tiles.TILE_SIZE + Tiles.TILE_SIZE / 2, y * Tiles.TILE_SIZE + Tiles.TILE_SIZE / 2));
                             Enemys.Add(enemy);
                             break;
