4e03b244f66f5687c614107a8f56cf6b8af04f27
Leon <baischl.tin24@student.dhbw-heidenheim.de>
2025-09-16 12:42:06 +0200

    Make the Goal class inherit from GameObject and not Enemy

--- DHBW-Game/GameObjects/Goal.cs
+++ DHBW-Game/GameObjects/Goal.cs
@@ -1,45 +1,32 @@
-﻿using System;
-using GameLibrary;
+﻿using GameLibrary;
 using GameLibrary.Entities;
 using GameLibrary.Graphics;
-using GameLibrary.Physics;
 using GameLibrary.Physics.Colliders;
-using GameLibrary.Rendering;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using MonoGameTutorial;
-using GameObjects.Animate;
 using DHBW_Game;
 using GameLibrary.Scenes;
 using DHBW_Game.Scenes;
 
-namespace GameObjects.Enemy;
+namespace GameObjects;
 
-public class Goal : Enemy
+public class Goal : GameObject
 {
     /// <summary>
-    /// Creates a new <see cref="Professor"/> object.
+    /// Creates a new <see cref="Goal"/> object.
     /// </summary>
-    /// <param name="mass">The mass of the physics component.</param>
-    /// <param name="isElastic">Whether the collider is elastic.</param>
-    public Goal(float mass, bool isElastic) : base (mass, isElastic)
+    public Goal()
     {
-        // Use circle collider
-        //Collider = new CircleCollider(this, new Vector2(0, 0), 30, isElastic);
-
         // Use rectangle collider
-        Collider = new RectangleCollider(this, new Vector2(0, 0), 4, 256, 0, isElastic);
+        Collider = new RectangleCollider(this, new Vector2(0, 0), 40, 256, 0, false);
         Collider.CollisionGroup = "enemy";
 
-        PhysicsComponent = new PhysicsComponent(this, mass);
-
-        ServiceLocator.Get<PhysicsEngine>().Add(PhysicsComponent);
+        ServiceLocator.Get<CollisionEngine>().Add(Collider);
     }
 
     /// <summary>
-    /// Initializes the test character at the given starting position in the world.
+    /// Initializes the <see cref="Goal"/> object at the given starting position in the world.
     /// </summary>
-    /// <param name="startingPosition">The position at which the test character should spawn.</param>
+    /// <param name="startingPosition">The position at which the <see cref="Goal"/> object should spawn.</param>
     public override void Initialize(Vector2 startingPosition)
     {
         base.Initialize(startingPosition);
@@ -49,7 +36,7 @@ public class Goal : Enemy
     {
         TextureAtlas goalAtlas = TextureAtlas.FromFile(Core.Content, "Static_Sprites/Goal-definition.xml");
 
-        // Create the closed door sprite
+        // Create the goal sprite
         var goalRegion = goalAtlas.GetRegion("goal");
         Sprite = new Sprite(goalRegion);
         Sprite.Scale = new Vector2(4.0f, 4.0f);
@@ -58,13 +45,12 @@ public class Goal : Enemy
     }
 
     /// <summary>
-    /// Updates the sprite which is set and also handles the input.
+    /// Updates the sprite which is set.
     /// </summary>
     /// <param name="gameTime">A snapshot of the timing values for the current update cycle.</param>
     public override void Update(GameTime gameTime)
     {
-        //base.Update(gameTime);
-
+        base.Update(gameTime);
     }
 
     /// <summary>
@@ -79,10 +65,8 @@ public class Goal : Enemy
     {
         base.TriggerCollision(collider);
 
-        if ((collider.GameObject is Player.Player) && (!_hasCollided))
+        if (collider.GameObject is Player.Player)
         {
-            _hasCollided = true;
-
             ServiceLocator.Get<Game1>().GameOver();
 
             GameScene scene = (GameScene)ServiceLocator.Get<Scene>();
